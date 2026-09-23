44bfc1d2f38e2825200eadbe3b949c01394078ae
abdo-div <abdulrahmanalhijaji@gmail.com>
2026-09-23 12:03:49 +0200

    doing the seconds

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -19,7 +19,7 @@ const getStripe = () => {
 
 const formatMoamalatDate = (date = new Date()) => {
   const pad = (n) => String(n).padStart(2, '0');
-  return `${date.getFullYear()}${pad(date.getMonth() + 1)}${pad(date.getDate())}${pad(date.getHours())}${pad(date.getMinutes())}`;
+  return `${date.getFullYear()}${pad(date.getMonth() + 1)}${pad(date.getDate())}${pad(date.getHours())}${pad(date.getMinutes())}${pad(date.getSeconds())}`;
 };
 
 export const createMoamalatCheckout = catchAsync(async (req, res, next) => {
